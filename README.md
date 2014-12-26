67ab0646f3246830f9aa1d767489ebd1030006b9
Kauê Gimenes <kaue@dot.net>
2014-12-26 10:30:03 -0200

    Add Login(email, pass, callback)

--- README.md
+++ README.md
@@ -2,3 +2,40 @@ plague-api
 ==========
 
 Plague Social Platform NodeJS API
+
+## Login(email, password, callback)
+```javascript
+plague.login('sample@domain.com', 'mypassword', function(user){
+  if(user.error){
+    console.log(user.error);
+    return;
+  }
+  console.log(user);
+})
+/*
+User Object:
+{
+  user: {
+    id,
+    avatar,
+    is_email_verified,
+    name,
+    karma,
+    can_infect,
+    avatar_200,
+    is_verified,
+    bio,
+    color,
+    is_temporary
+  },
+  client: {
+    id,
+    uid,
+    token,
+    locale,
+    useragent,
+    ipaddress
+  }
+}
+*/
+```
--- plague-api.js
+++ plague-api.js
@@ -1,4 +1,57 @@
-'use strict'
+(function () {
+  var plague = {};
+  var request = require('request'),
+      util    = require('util');
 
 
-var request = require('request');
+
+  var apiToken,
+      apiUserId;
+  var apiHeaders = {
+    'Host':'plague.io',
+    'Content-Type':'application/x-www-form-urlencoded; charset=utf-8',
+    'Connection':'keep-alive',
+    'Proxy-Connection':'keep-alive',
+    'Accept':'application/json',
+    'User-Agent':'Plague/1.1.25 (iPhone; iOS 8.3; Scale/2.00)',
+    'Accept-Language':'en',
+    'Accept-Encoding':'gzip, deflate'
+  };
+
+  /*
+    Todo
+    http://plague.io/api/auth/confirm_email/?recipient=EMAIL&code=CODE
+    http://plague.io/api/auth/reset_password/?recipient=EMAIL&code=CODE
+  */
+  //
+  // Login to get UserId and Token
+  //
+  plague.login = function(email, password, callback){
+    var requestUrl = util.format("http://plague.io/api/auth/login/?email=%s&password=%s", email, password);
+    request({
+      url: requestUrl,
+      headers: apiHeaders
+    }, function(error, response, body) {
+      if (!error) {
+        var response = JSON.parse(body);
+        callback(response) // Show the HTML for the Google homepage.
+      }
+    });
+  };
+
+  // Node.js
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = plague;
+  }
+  // AMD / RequireJS
+  else if (typeof define !== 'undefined' && define.amd) {
+    define([], function () {
+      return plague;
+    });
+  }
+  // included directly via <script> tag
+  else {
+    root.async = plague;
+  }
+
+}());
