925400996b567a72d7a9d6665d17fcefe631ea09
Kauê Gimenes <kaue@dot.net>
2014-12-26 11:37:45 -0200

    Include Plague Functions

--- plague-api.js
+++ plague-api.js
@@ -15,15 +15,20 @@
     'Accept-Language':'en',
     'Accept-Encoding':'gzip, deflate'
   };
-
+  var options = {};
+  var auth = {};
   /*
     Todo
     http://plague.io/api/auth/confirm_email/?recipient=EMAIL&code=CODE
     http://plague.io/api/auth/reset_password/?recipient=EMAIL&code=CODE
   */
-  //
-  // Login to get UserId and Token
-  //
+  plague.set = function(setOptions){
+    options = setOptions;
+    return plague;
+  }
+  /*
+    Login to get UserId and Token
+  */
   plague.login = function(email, password, callback){
     var requestUrl = util.format("http://plague.io/api/auth/login/?email=%s&password=%s", email, password);
     request({
@@ -32,15 +37,19 @@
     }, function(error, response, body) {
       if (!error) {
         var response = JSON.parse(body);
-        callback(response) // Show the HTML for the Google homepage.
+        if(response.client.uid)
+          auth.uid = response.client.uid;
+        if(response.client.token)
+          auth.token = response.client.token;
+        callback(response);
       }
     });
   };
-  //
-  // Return all user posts
-  //
-  plague.getPosts = function(uid, token, callback){
-    var requestUrl = util.format("http://plague.io/api/posts/?uid=%s&token=%s",uid, token);
+  /*
+    Return all user posts
+  */
+  plague.getPosts = function(callback){
+    var requestUrl = util.format("http://plague.io/api/posts/?uid=%s&token=%s",auth.uid, auth.token);
     request({
       url: requestUrl,
       headers: apiHeaders
@@ -51,14 +60,62 @@
       }
     });
   };
-  //
-  // Return Nearby Plagues
-  //
-  plague.getInfectionsNearby = function(uid, token, callback){
-    var requestUrl = util.format("http://plague.io/api/infections/?uid=%s&token=%s",uid, token);
+  /*
+    Return Nearby Plagues
+  */
+  plague.getInfectionsNearby = function(callback){
+    var requestUrl = util.format("http://plague.io/api/infections/?uid=%s&token=%s",auth.uid, auth.token);
     request({
       url: requestUrl,
       headers: apiHeaders
+    }, function(error, response, body) {
+      if (!error) {
+        var response = JSON.parse(body);
+        callback(response) // Show the HTML for the Google homepage.
+      }
+    });
+  };
+  /*
+    Send a Text Post to Plague
+  */
+  plague.postText = function(text, callback){
+    var requestUrl = 'http://plague.io/api/posts/';
+    request.post({
+      url: requestUrl,
+      headers: apiHeaders,
+      form: {
+        latitude: options.latitude,
+        longitude: options.longitude,
+        meta:'{"administrativeArea":"Quebec","country":"Canada","locality":"Montreal"}',
+        text: text,
+        token: auth.token,
+        uid: auth.uid
+      }
+    }, function(error, response, body) {
+      if (!error) {
+        var response = JSON.parse(body);
+        callback(response) // Show the HTML for the Google homepage.
+      }
+    });
+  };
+  /*
+    Send a Post with a Media Link
+  */
+  plague.postLink = function(mediaLink, mediaLinkPreview, text, callback){
+    var requestUrl = 'http://plague.io/api/posts/';
+    request.post({
+      url: requestUrl,
+      headers: apiHeaders,
+      form: {
+        latitude: options.latitude,
+        longitude: options.longitude,
+        meta:'{"administrativeArea":"Quebec","country":"Canada","locality":"Montreal"}',
+        media: mediaLink,
+        media_preview: mediaLinkPreview,
+        text: text,
+        token: auth.token,
+        uid: auth.uid
+      }
     }, function(error, response, body) {
       if (!error) {
         var response = JSON.parse(body);
